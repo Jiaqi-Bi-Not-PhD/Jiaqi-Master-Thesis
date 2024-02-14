709f0f6594a6cabe804ca4d52cd21bb0cf3d9fb1
Jiaqi-Bi-Not-PhD <garybi431@gmail.com>
2024-02-13 19:55:17 -0500

    feb 13

--- MCEM.R
+++ MCEM.R
@@ -202,7 +202,7 @@ mcem_step <- function(data, initial_theta,
     imputed_datasets <- mclapply(1:m_imputations, function(i) {
       imputed_data <- data
       
-      lmer_model <- lmer(PRS ~ log(timeBC)*BC + currentage + proband + mgeneI + (1|famID), data = imputed_data, REML = FALSE)
+      lmer_model <- lmer(PRS ~ log(timeBC):BC + currentage + proband + mgeneI + (1|famID), data = imputed_data, REML = FALSE)
       sigma1 <- sigma(lmer_model)
       
       for (g in 1:(n_gibbs + burn_in)) { ## burn-in iterations
@@ -238,36 +238,21 @@ mcem_step <- function(data, initial_theta,
       }, mc.cores = parallel::detectCores())))
     }
     
-    ## E-step Gamma
-    #objective_function <- function(theta) {
-    #  mean(unlist(mclapply(imputed_datasets, function(dataset) {
-    #    Y <- as.matrix(dataset[, c("timeBC", "BC")])
-    #    X <- as.matrix(dataset[, c("PRS", "mgeneI")])
-    #    loglik_frailty_single_gamma(X = X, Y = Y, theta = theta, 
-    #                                data = dataset, nbase = 2, 
-    #                                base.dist = "Weibull", 
-    #                                frailty.dist = "gamma", agemin = 18,
-    #                                design = design)
-    #  }, mc.cores = parallel::detectCores())))
-    #}
+
     
     ## M-step: Gamma and log-normal, Nelder-Mead
     theta_history[[iter + 1]] <- current_theta
     optimized_result <- optim(par = current_theta, objective_function)
     current_theta <- optimized_result$par
     
-    ## Convergence rule
-    if (sum(abs(current_theta - prev_theta) < tol) == length(initial_theta)) {
-      converged <- TRUE
-    } else {
-      prev_theta <- current_theta
-    }
+  
+    prev_theta <- current_theta ## Give it no convergence rule
     
     iter <- iter + 1
   }
   
   return(list(final_theta = current_theta,
-              convergence = converged,
+              convergence = FALSE,
               iterations = iter,
               Baseline = "Weibull", 
               Frailty_Dist = frailty.dist,
@@ -281,6 +266,33 @@ results_gamma <- mcem_step(data = brca1_prs_MCEM, initial_theta = initial_params
 results_lognormal <- mcem_step(data = brca1_prs_MCEM, initial_theta = initial_params, 
                                design = "pop", frailty.dist = "lognormal", m_imputations = 5,
                                n_gibbs = 5, burn_in = 5)
+
+## E-step Gamma
+#objective_function <- function(theta) {
+#  mean(unlist(mclapply(imputed_datasets, function(dataset) {
+#    Y <- as.matrix(dataset[, c("timeBC", "BC")])
+#    X <- as.matrix(dataset[, c("PRS", "mgeneI")])
+#    loglik_frailty_single_gamma(X = X, Y = Y, theta = theta, 
+#                                data = dataset, nbase = 2, 
+#                                base.dist = "Weibull", 
+#                                frailty.dist = "gamma", agemin = 18,
+#                                design = design)
+#  }, mc.cores = parallel::detectCores())))
+#}
+
+## Convergence rule
+#if (sum(abs(current_theta - prev_theta) < tol) == length(initial_theta)) {
+#  converged <- TRUE
+#} else {
+#  prev_theta <- current_theta
+#}
+
+## Convergence rule (use loglikelihood value)
+# if (sum(abs(current_theta - prev_theta) < tol) == length(initial_theta)) {
+#  converged <- TRUE
+#} else {
+prev_theta <- current_theta ## Give it no convergence rule
+#}
 
 ## Diagnostics
 theta_matrix <- do.call(rbind, results_gamma$theta_history)
@@ -302,5 +314,113 @@ par(mfrow = c(3, 2))
 for (i in 1:ncol(theta_matrix)) {
   acf(theta_matrix[, i], main = paste("ACF for Parameter", i))
 }
+
+#######  MCEM for Gamma & log-normal (Gibb's Sampling, modelling lmer)  ############
+library(foreach)
+mcem_step <- function(data, initial_theta, 
+                      design, 
+                      m_imputations = 20, tol = 1e-6, 
+                      max_iter = 1000, n_gibbs = 10, burn_in = 5,
+                      frailty.dist) {
+  current_theta <- initial_theta
+  converged <- FALSE
+  iter <- 0
+  
+  ## Store the history theta to plot the diagnostics
+  theta_history <- list()
+  
+  ## Initialize a structure to hold the last state of imputed data for each imputation
+  last_imputed_state <- vector("list", m_imputations)
+  
+  imputed_data <- data
+  
+  while (!converged & iter < max_iter) {
+    ## Gibb's sampling with conditioning on the last state
+    imputed_datasets <- mclapply(1:m_imputations, function(i) {
+      
+      ## Fit the mixed-effects model on the current imputed data
+      lmer_model <- lmer(PRS ~ log(timeBC):BC + currentage + proband + mgeneI + (1|famID), data = imputed_data, REML = FALSE)
+      sigma1 <- sigma(lmer_model)
+      
+      ## Perform Gibbs sampling
+      for (g in 1:(n_gibbs + burn_in)) { ## burn-in iterations
+        foreach(family = unique(imputed_data$famID), .combine = 'rbind') %dopar% { ## Family-wise
+          family_data <- subset(imputed_data, famID == family)
+          missing_prs <- is.na(family_data$PRS)
+          
+          if (any(missing_prs)) { ## Model the missing PRS
+            for (j in which(missing_prs)) { ## Within-family
+              mu <- predict(lmer_model, newdata = family_data[j, ], re.form = NULL, type = "response")
+              if (g > burn_in) {
+                family_data$PRS[j] <- rnorm(1, mean = mu, sd = sigma1)
+              }
+            }
+          } ## Model the missing PRS
+        } ## Family-wise
+      } ## burn-in iterations
+      
+      ## Update the last state for this imputation
+      last_imputed_state[[i]] <- imputed_data
+      return(imputed_data)
+    }, mc.cores = parallel::detectCores())
+    
+    ## Store the history thetas
+    theta_history[[iter+1]] <- current_theta
+    
+    ## E-step Log-Normal
+    objective_function <- function(theta) {
+      mean(unlist(mclapply(imputed_datasets, function(dataset) {
+        Y <- as.matrix(dataset[, c("timeBC", "BC")])
+        X <- as.matrix(dataset[, c("PRS", "mgeneI")])
+        lognormal_single(X = X, Y = Y, theta = theta, 
+                         data = dataset, nbase = 2, 
+                         base.dist = "Weibull", 
+                         frailty.dist = "lognormal", agemin = 18,
+                         design = design)
+      }, mc.cores = parallel::detectCores())))
+    }
+    
+    
+    
+    ## M-step: Gamma and log-normal, Nelder-Mead
+    theta_history[[iter + 1]] <- current_theta
+    optimized_result <- optim(par = current_theta, objective_function)
+    current_theta <- optimized_result$par
+    
+    
+   
+    
+    ## Implement MC-based stopping rule here
+    if (length(theta_history) >= 21) {
+      squared_diffs <- sapply(1:length(current_theta), function(k) {
+        (theta_history[[iter + 1]][k] - theta_history[[iter - 19]][k])^2
+      })
+      if (all(squared_diffs < tol)) {
+        converged <- TRUE
+      }
+      else prev_theta <- current_theta ## Update theta
+    }
+    
+    
+    iter <- iter + 1
+    
+  }
+  
+  return(list(final_theta = current_theta,
+              convergence = converged,
+              iterations = iter,
+              Baseline = "Weibull", 
+              Frailty_Dist = frailty.dist,
+              theta_history = theta_history))
+}
+
+
+initial_params <- c(1/41.41327,1,0, 0, 1)
+results_gamma <- mcem_step(data = brca1_prs_MCEM, initial_theta = initial_params,
+                           design = "pop", frailty.dist = "gamma", m_imputations = 20,
+                           n_gibbs = 5, burn_in = 5)
+results_lognormal <- mcem_step(data = brca1_prs_MCEM, initial_theta = initial_params, 
+                               design = "pop", frailty.dist = "lognormal", m_imputations = 20,
+                               n_gibbs = 5, burn_in = 5)
 
 
