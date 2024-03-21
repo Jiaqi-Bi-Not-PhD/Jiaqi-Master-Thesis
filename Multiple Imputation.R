4b210e96350d65b0b127af256592e9771b4ee07b
Jiaqi Bi <garybi431@gmail.com>
2024-03-20 22:32:10 -0400

    Mar 20

--- Multiple Imputation.R
+++ Multiple Imputation.R
@@ -2,11 +2,13 @@
 ########### Multiple Imputation ##########################
 ##########################################################
 ## Kinship matrix
+devtools::install_github("variani/lme4qtl")
 library(kinship2)
 library(Matrix)
-library(lme4qtl)
+library("lme4qtl")
 library(lmerTest)
 library(parallel)
+library(MASS)
 
 kinship_mat <- with(brca1_prs, kinship(id = indID, dadid = fatherID, momid = motherID,
                                        sex = rep(2,nrow(brca1_prs))))
@@ -25,15 +27,21 @@ sigma_g <- attr(VarCorr(model_test)$indID, "stddev")
 sigma_e <- attr(VarCorr(model_test), "sc")
 K <- sigma_g^2 * as(kinship_mat, "sparseMatrix") + sigma_e^2
 K <- as.matrix(K)
+kinship_ids <- rownames(K)
 
 ## Imputation step
 numCores <- detectCores() - 2
-
-impute_PRS_dataset <- function(dataset, mean_PRS, Sigma, seed) {
-  set.seed(123)  # Ensure reproducibility
+imputed_data <- list(brca1_prs)
+impute_PRS_dataset <- function(dataset, mean_PRS, Sigma, seed) { 
   
-  ## PRS has NAs
-  ## Logic
+  missing_indices <- brca1_prs[which(brca1_prs$miss_index == 1), "indID"]
+  for (i in missing_indices) {
+    K_i <- K[i, i]  
+    mu_i <- mean_PRS[i] # Need to define
+    
+    imputed_value <- mvrnorm(n = 1, mu = mu_i, Sigma = matrix(K_i, nrow = 1))
+    imputed_data$PRS[i] <- imputed_value
+  }
 
   return(imputed_dataset)
 }
